f7065948eb90b1af19712a3ea0cff3320354753e
Sahil Anguralla <sahil.anguralla@inmobi.com>
2019-01-09 10:46:54 +0530

    0.0.13 - version bump and added build files

--- dist/slect.js
+++ dist/slect.js
@@ -151,14 +151,31 @@ var GeneralUtils = /** @class */ (function () {
             longer = s2;
             shorter = s1;
         }
-        if (!exactMatch) {
-            longer = longer.slice(0, shorter.length);
-        }
         var longerLength = longer.length;
         if (longerLength == 0) {
             return 1.0;
         }
-        return ((longerLength - this.editDistance(longer, shorter)) / longerLength);
+        debugger;
+        var matchScore = (longerLength - this.editDistance(longer, shorter)) / longerLength;
+        if (!exactMatch) {
+            var slicedLonger = longer.slice(0, shorter.length);
+            var slicedLongerLength = slicedLonger.length;
+            var newMatchScore = ((slicedLongerLength -
+                this.editDistance(slicedLonger, shorter)) /
+                slicedLongerLength) *
+                (shorter.length / slicedLonger.length);
+            if (newMatchScore > matchScore) {
+                if (newMatchScore === 1)
+                    matchScore = newMatchScore;
+                else {
+                    newMatchScore =
+                        newMatchScore * (slicedLongerLength / longerLength);
+                    matchScore =
+                        matchScore > newMatchScore ? matchScore : newMatchScore;
+                }
+            }
+        }
+        return matchScore;
     };
     GeneralUtils.editDistance = function (s1, s2) {
         s1 = s1.toLowerCase();
@@ -346,6 +363,9 @@ var Slect = /** @class */ (function () {
         this.inputEl.addEventListener('keyup', this.onInputKeyUp);
         this.inputEl.addEventListener('keydown', this.onInputKeyDown);
         this.inputEl.addEventListener('focus', this.onFocus);
+        var separator = document.createElement('div');
+        HTMLElementUtils_1.default.addClass(separator, 'slect-input-separator');
+        this.element.appendChild(separator);
         this.suggestionList.render().then(function (el) { return _this.element.appendChild(el); });
         var slectActionsContainer = document.createElement('div');
         HTMLElementUtils_1.default.addClass(slectActionsContainer, 'slect-actions-container');
@@ -460,7 +480,7 @@ var Slect = /** @class */ (function () {
     };
     Object.defineProperty(Slect, "version", {
         get: function () {
-            return "v0.0.11-1-g0575c28";
+            return "v0.0.12-2-g2265f1d";
         },
         enumerable: true,
         configurable: true
@@ -475,9 +495,9 @@ var Slect = /** @class */ (function () {
                 console.info('onSelect is not handled. You can do the same by passing via config', options);
         },
         get placeholder() {
-            return this.allowCustomOption
-                ? 'Please enter...'
-                : 'Please choose...';
+            return this.allowViewAllOptions
+                ? 'Type or select an option'
+                : 'Please type';
         },
         allowCustomOption: false
     };
@@ -649,7 +669,7 @@ module.exports = "<svg viewBox=\"0 0 20 20\" aria-hidden=\"true\" focusable=\"fa
 /* 9 */
 /***/ (function(module, exports) {
 
-module.exports = "<svg viewBox=\"0 0 20 20\" aria-hidden=\"true\" focusable=\"false\" class=\"css-19bqh2r\"><path d=\"M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z\"></path></svg>"
+module.exports = "<svg version=\"1.1\" id=\"Capa_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" viewBox=\"0 0 292.362 292.362\" style=\"enable-background:new 0 0 292.362 292.362;\" xml:space=\"preserve\"><g><path d=\"M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424 C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428 s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z\"></path></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>"
 
 /***/ })
 /******/ ])["default"];
